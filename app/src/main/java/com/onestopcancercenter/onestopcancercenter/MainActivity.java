8b3aeba6f11e507261b0c12d6c6366414671a035
norah 3mair <norah3mair@MacBook-Air-alkhas-b-norah.local>
2018-02-02 12:48:05 +0300

    norah update

--- app/src/main/java/com/onestopcancercenter/onestopcancercenter/MainActivity.java
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/MainActivity.java
@@ -3,11 +3,32 @@ package com.onestopcancercenter.onestopcancercenter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView IV = (ImageView) findViewById(R.id.imageView3);
+        final Intent intent= new Intent(this, user_type_page.class);
+        IV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
