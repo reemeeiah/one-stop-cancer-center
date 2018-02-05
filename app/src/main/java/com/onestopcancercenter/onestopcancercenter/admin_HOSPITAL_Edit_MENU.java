c38a68a8aaf45e528f5064251b0aec7d3fa9f7df
reemeeiah <36019436+reemeeiah@users.noreply.github.com>
2018-02-05 19:00:08 +0300

    'reemeeiah'

---
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/admin_HOSPITAL_Edit_MENU.java
@@ -0,0 +1,13 @@
+package com.onestopcancercenter.onestopcancercenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class admin_HOSPITAL_Edit_MENU extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__hospital__edit__menu);
+    }
+}
