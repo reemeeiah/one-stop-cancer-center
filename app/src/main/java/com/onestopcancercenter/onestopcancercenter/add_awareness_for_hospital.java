d0b844d7adb46887a5720b49e1afcad28e76a271
reemeeiah <36019436+reemeeiah@users.noreply.github.com>
2018-02-06 19:00:40 +0300

    'reemeeiah'

---
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/add_awareness_for_hospital.java
@@ -0,0 +1,13 @@
+package com.onestopcancercenter.onestopcancercenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_awareness_for_hospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_awareness_for_hospital);
+    }
+}
