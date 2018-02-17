a1e3ea921b7bd59299c06ec80bdc18b814f71e14
Nawal-ir <nawalir.16@gmail.com>
2018-02-17 23:33:29 +0300

    Some Changes

---
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/User_type_page.java
@@ -0,0 +1,14 @@
+package com.onestopcancercenter.onestopcancercenter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class User_type_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type_page);
+    }
+}
+
