e8c5b6838aa0da34312b2f582750386531479d03
wedadsaud1 <36005441+wedadsaud1@users.noreply.github.com>
2018-02-17 20:49:01 +0300

    new activity

---
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/user_sign_up_page.java
@@ -0,0 +1,13 @@
+package com.onestopcancercenter.onestopcancercenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class user_sign_up_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_up_page);
+    }
+}
