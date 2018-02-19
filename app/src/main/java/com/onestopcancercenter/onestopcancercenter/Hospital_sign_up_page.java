5d5b9e9d62bfb24beead1365ed7dccd51efc8d6b
wedadsaud1 <36005441+wedadsaud1@users.noreply.github.com>
2018-02-19 17:08:30 +0300

    وداد تعديل

---
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/Hospital_sign_up_page.java
@@ -0,0 +1,13 @@
+package com.onestopcancercenter.onestopcancercenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hospital_sign_up_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_sign_up_page);
+    }
+}
