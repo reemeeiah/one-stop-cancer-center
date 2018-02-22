c79db5804623c7e7a186a3bf034f42737be158b0
reemeeiah <36019436+reemeeiah@users.noreply.github.com>
2018-02-22 18:03:44 +0300

    sign in for admin & user

--- app/src/main/java/com/onestopcancercenter/onestopcancercenter/Admin_Sign_in.java
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/Admin_Sign_in.java
@@ -1,13 +1,50 @@
 package com.onestopcancercenter.onestopcancercenter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Admin_Sign_in extends AppCompatActivity {
+    private EditText name;
+    private EditText password;
+    private TextView info;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__sign_in);
+        name =(EditText) findViewById(R.id.adminlogin);
+        password =(EditText) findViewById(R.id.password2);
+        info=(TextView ) findViewById(R.id.textView11);
+        login=(Button)findViewById(R.id. button8);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(),password.getText().toString());
+            }
+        });
+
     }
-}
+
+    private void validate (String username , String userpassword){
+        if ((username.equals("admin")) && (userpassword.equals("1234"))){
+
+            Intent intent= new Intent(Admin_Sign_in.this,Homepage.class);
+            startActivity(intent);
+        }
+        else{
+
+            info.setText(" Wrong password");
+
+            }
+        }
+
+
+    }
+
--- app/src/main/java/com/onestopcancercenter/onestopcancercenter/user_sign_in_page.java
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/user_sign_in_page.java
@@ -1,13 +1,55 @@
 package com.onestopcancercenter.onestopcancercenter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class user_sign_in_page extends AppCompatActivity {
-
+private EditText name;
+private EditText password;
+private TextView info;
+private Button login;
+private  int counter=5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_in_page);
+
+    name =(EditText) findViewById(R.id.textView_username);
+    password =(EditText) findViewById(R.id.password1);
+    info=(TextView ) findViewById(R.id.textView10);
+    login=(Button)findViewById(R.id. button_login);
+    info.setText("Number of attemps remaining: 5");
+login.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+validate(name.getText().toString(),password.getText().toString());
     }
+});
+}
+
+private void validate (String username , String userpassword){
+      if ((username.equals("user")) && (userpassword.equals("1234"))){
+
+         Intent intent= new Intent(user_sign_in_page.this,Homepage.class);
+startActivity(intent);
+}
+else{
+          counter--;
+          info.setText(" Number of attemps remaining:"+String.valueOf(counter));
+          if (counter==0){
+              login.setEnabled(false);
+          }
+      }
+
+
+}
+
+
+
+
 }
