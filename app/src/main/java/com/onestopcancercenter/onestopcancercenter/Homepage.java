c2bfc3b25c586bfc972d179b9c587b4ec58a903f
norah3mair <alomair.norah@gmail.com>
2018-02-24 10:17:57 +0300

    UPDATE NORAH OMAIR 2

--- app/src/main/java/com/onestopcancercenter/onestopcancercenter/Homepage.java
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/Homepage.java
@@ -2,6 +2,9 @@ package com.onestopcancercenter.onestopcancercenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class Homepage extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+
+    String s = "/data/data" + Homepage.this.getPackageName() + "/Database/";
+
+    DatabaseHelper DB = new DatabaseHelper(Homepage.this, s);
+    try
+    {
+        DB.prepareDatabase();
     }
+    catch(
+    IOException e)
+    {
+        e.printStackTrace();
+    }
+
+
+}
+
 }
