db64f4c3e2737589d854518aaac0f6829219bacc
reemeeiah <36019436+reemeeiah@users.noreply.github.com>
2018-03-02 01:06:02 +0300

    add cancer

--- app/src/main/java/com/onestopcancercenter/onestopcancercenter/add_cancer.java
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/add_cancer.java
@@ -8,22 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class add_cancer extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_cancer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
+
+
+
+
