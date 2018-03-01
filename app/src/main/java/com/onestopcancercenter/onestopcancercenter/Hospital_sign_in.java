e5ca83be2dc8614c1584f02af215836bfa89618b
Nawal's <Master21>
2018-03-01 15:05:49 +0300

    Hospital Sign in

---
+++ app/src/main/java/com/onestopcancercenter/onestopcancercenter/Hospital_sign_in.java
@@ -0,0 +1,13 @@
+package com.onestopcancercenter.onestopcancercenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hospital_sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_sign_in);
+    }
+}
